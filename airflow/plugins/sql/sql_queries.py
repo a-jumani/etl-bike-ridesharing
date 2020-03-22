1a2363199e3d17bd960d737340eaeea9c98bc535
a-jumani <arsalan.jumani@outlook.com>
2020-03-22 21:14:48 +0400

    feat: Add weather data staging table schemas

--- airflow/plugins/sql/sql_queries.py
+++ airflow/plugins/sql/sql_queries.py
@@ -3,11 +3,17 @@ class SqlInitTables:
     drop_staging_tables = """
         DROP TABLE IF EXISTS public.staging_bike_rides;
         DROP TABLE IF EXISTS public.staging_holiday;
+        DROP TABLE IF EXISTS public.staging_temperature;
+        DROP TABLE IF EXISTS public.staging_humidity;
+        DROP TABLE IF EXISTS public.staging_weather_desc;
     """
 
     clear_staging_tables = """
         DELETE FROM public.staging_bike_rides;
         DELETE FROM public.staging_holiday;
+        DELETE FROM public.staging_temperature;
+        DELETE FROM public.staging_humidity;
+        DELETE FROM public.staging_weather_desc;
     """
 
     create_staging_tables = """
@@ -27,5 +33,20 @@ class SqlInitTables:
             day     VARCHAR (10) NOT NULL,
             date    VARCHAR (15) NOT NULL,
             holiday VARCHAR (50) NOT NULL
+        );
+
+        CREATE TABLE IF NOT EXISTS public.staging_temperature (
+            datetime TIMESTAMP NOT NULL,
+            value    NUMERIC (7, 3)
+        );
+
+        CREATE TABLE IF NOT EXISTS public.staging_humidity (
+            datetime TIMESTAMP NOT NULL,
+            value    NUMERIC (5, 1)
+        );
+
+        CREATE TABLE IF NOT EXISTS public.staging_weather_desc (
+            datetime TIMESTAMP NOT NULL,
+            value    VARCHAR (50)
         );
     """
