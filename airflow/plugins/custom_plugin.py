4cae723be2ac550d04ebca001185a021d2c0478c
a-jumani <arsalan.jumani@outlook.com>
2020-03-24 12:27:12 +0400

    feat: Add postgres check operator

--- airflow/plugins/custom_plugin.py
+++ airflow/plugins/custom_plugin.py
@@ -1,11 +1,13 @@
 from airflow.plugins_manager import AirflowPlugin
 
 from operators.stage_redshift import StageRedshiftOperator
+from operators.data_quality import PostgresCheckOperator
 
 
 # Defining the plugin class
 class CustomPlugin(AirflowPlugin):
     name = "custom_plugin"
     operators = [
-        StageRedshiftOperator
+        StageRedshiftOperator,
+        PostgresCheckOperator
     ]
---
+++ airflow/plugins/operators/data_quality.py
@@ -0,0 +1,30 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.check_operator import CheckOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class PostgresCheckOperator(CheckOperator):
+    """ Performs check against a Postgres database. It expects an SQL query
+    and checks if first row of result of query has values which python-eval to
+    False. If yes, the operator fails. It passes otherwise.
+    """
+
+    @apply_defaults
+    def __init__(self,
+                 sql,
+                 postgres_conn_id,
+                 *args, **kwargs):
+        """
+        Args:
+            sql SQL query, ideally resulting in a 1-row result
+            postgres_conn_id Conn Id of the database credentials
+        Preconditions:
+            sql must give non-empty result
+        Caution:
+            only 1st row of the given SQL query which be checked
+        """
+        super(PostgresCheckOperator, self).__init__(sql=sql, *args, **kwargs)
+        self.postgres_conn_id = postgres_conn_id
+
+    def get_db_hook(self):
+        return PostgresHook(self.postgres_conn_id)
